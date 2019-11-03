e9223a54695be367eeb1cda06a405e273dd47f3f
nathanjohnsonwolfbotics <nathanieljohnson009@gmail.com>
2019-11-03 14:55:10 -0800

    Removed unused imports and declarations in MasterFunction.java file.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MasterFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MasterFunctions.java
@@ -1,14 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
-
-import java.lang.Math;
-
 public class MasterFunctions {
-
-    DcMotor leftWheel;
-    DcMotor rightWheel;
-
+    
     public static int inchesToTicks(double inches) {
         return (int) ((960/(4*Math.PI)) * inches);
     }
